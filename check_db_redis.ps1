1f70bcb16c4faa63c3667cd2db2f07fe6a14b842
TotoMacho <anthony.ouinet@viacesi.fr>
2017-12-18 17:25:48 +0000

    Add files via upload

--- check_db_redis.ps1
+++ check_db_redis.ps1
@@ -1,7 +1,7 @@
 ﻿$Error.Clear()
 Connect-RedisServer -Host 192.168.139.140 -Port 6379
 $test = $Error[-1]
-Send-RedisCommand -Command "auth 9Droviof"
+Send-RedisCommand -Command "auth Passwd"
 $var = Send-RedisCommand -Command "PING"
 If($var -eq "PONG"){
     Write-Host "OK- Connection to Redis database established"
