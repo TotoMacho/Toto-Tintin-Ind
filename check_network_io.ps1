8d62d91bf6a4797f4f7d5b8e481298acb016dd11
Anthony.Ouinet <anthony.ouinet@viacesi.fr>
2017-12-20 10:20:26 +0000

    check_network_io

---
+++ check_network_io.ps1
@@ -0,0 +1,64 @@
+﻿$t = netstat -e 
+
+foreach($line in $t[5]){
+$line = $line -replace '^\s+',''
+$line = $line -split '\s+'
+ $properties = @{
+        Name = $line[0..1]
+        ReceivedPacket = $line[2]
+        SendPacket = $line[3]
+    }
+
+    $Packet = New-Object -TypeName PSObject -Property $properties 
+    
+
+}
+foreach($line in $t[7]){
+$line = $line -replace '^\s+',''
+$line = $line -split '\s+'
+ $properties = @{
+        Name = $line[0]
+        ReceivedReject = $line[1]
+        SendReject = $line[1]
+    }
+
+    $Reject = New-Object -TypeName PSObject -Property $properties 
+    
+
+}
+foreach($line in $t[8]){
+$line = $line -replace '^\s+',''
+$line = $line -split '\s+'
+ $properties = @{
+        Name = $line[0]
+        ReceivedErrors = $line[1]
+        SendErrors = $line[1]
+    }
+
+    $Errors = New-Object -TypeName PSObject -Property $properties 
+    
+
+}
+$PctRSuccess = $Packet.ReceivedPacket / ($Packet.ReceivedPacket - $Reject.ReceivedReject - $Errors.ReceivedErrors ) * 100
+$PctRFailed = 100 - $PctRSuccess
+
+if($PctRSuccess -gt 75){
+
+    Write-Host "OK- % of successfully received packet is $PctRSuccess%"
+    exit 0
+
+}elseif($PctRSuccess -lt 75){
+
+    Write-Host "WARNING- $PctRFailed% of packets are lost"
+    exit 1
+
+}elseif($PctRSucces -lt 50){
+
+    Write-Host "CRITICAL- $PctRFailed% of packets are lost"
+    exit 2
+
+}else{
+
+    Write-Host "UNKNOWN- No information about packet lost"
+    exit 3
+}
